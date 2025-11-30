75184c0bf389e700360f96acfa640f8d949ae081
Checkey01 <Chiaki2048@outlook.com>
2025-11-30 20:29:25 +0800

    test: 在测试root_url时会输出当前环境下期望的root_url的绝对路径

--- test/test_settings.py
+++ test/test_settings.py
@@ -5,4 +5,5 @@ from app.core.config import Settings
 def test_settings_root_url():
     settings = Settings()
     root_url = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    print(f'target_root_url = "{root_url}"')
     assert settings.root_url == root_url
