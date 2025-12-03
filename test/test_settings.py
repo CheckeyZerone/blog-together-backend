1fde6920499c754a342f29f0226648e40ec4ac06
Checkey01 <Chiaki2048@outlook.com>
2025-12-03 17:27:43 +0800

    test: 添加了针对database_url的测试

--- test/test_settings.py
+++ test/test_settings.py
@@ -1,9 +1,23 @@
 import os.path
+from unittest import TestCase
 
 from app.core.config import Settings
 
-def test_settings_root_url():
+
+class SettingsTest(TestCase):
     settings = Settings()
-    root_url = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    print(f'target_root_url = "{root_url}"')
-    assert settings.root_url == root_url
+
+    def test_settings_root_url(self):
+        settings = self.settings
+        root_url = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+        print(f'target_root_url = "{root_url}"')
+        assert settings.root_url == root_url
+
+    def test_settings_database_url(self):
+        settings = self.settings
+        database_url = settings.database_url
+        print(f'target_database_url = "{database_url}"')
+        if isinstance(database_url, str):
+            assert database_url.startswith("sqlite+aiosqlite://")
+        else:
+            assert database_url.port is None
